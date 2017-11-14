594f61209bd3e85c939a488b8870842fa405ce74
Unknown <sadzot.antoine@gmail.com>
2017-11-14 11:45:09 +0100

    uffindlist presque complet
    ssqdfssdsd

--- src/libs/union-find/UnionFindList.c
+++ src/libs/union-find/UnionFindList.c
@@ -8,6 +8,7 @@ typedef struct {
 typedef struct {
     struct Element* first;
     struct Element* last;
+    int numberElements;
 }Sentinel;
 struct union_find_t{
     struct Element* elements;
@@ -27,7 +28,56 @@ UnionFind* ufCreate(size_t n_items)
         unionFind->elements[i].head = &unionFind->sentinels[i];
         unionFind->sentinels[i].first = &unionFind->elements[i];
         unionFind->sentinels[i].last = &unionFind->elements[i];
+        unionFind->sentinels[i].numberElements = 1;
     }
-    return unionFind;
+    return &unionFind;
 }
 
+void ufFree(UnionFind* union_find)
+{
+    free(unionFind->sentinels);
+    free(unionFind->elements);
+    free(unionFind);
+}
+
+size_t ufFind(const UnionFind* union_find, size_t item)
+{
+    //takes the head element pointed by the sentinel of the item transmited as argument.
+    return union_find->elements[item].head->first->numero;
+}
+
+ufStatus ufUnion(UnionFind* union_find, size_t item1, size_t item2)
+{
+    if (ufFind(&union_find, item1) == ufFind(&union_find, item2))
+        return UF_SAME;
+    else 
+    {
+        size_t toConcat, toHost;
+        //the subset containing the least elements is merged to the other subset.
+       if (union_find->elements[item1].head->numberElements <= union_find->elements[item2].head->numberElements)
+        {toConcat = item1;
+         toHost = item2;}
+        else
+        {toConcat = item2;
+         toHost = item1;}
+        union_find->elements[toHost].head->last->(*next) = union_find->elements[toConcat].head->(&first);
+        union_find->elements[toHost].head->(*last) = union_find->elements[toConcat].head->(&last);
+        struct Element* pointToHead;
+        struct Sentinel* pointToSentinel;
+        pointToHead = union_find->elements[toConcat].head.first;
+        pointToSentinel = union_find->elements[toConcat].head;
+
+       
+        for(size_t i = 0; i < union_find->elements[toConcat].head.numberElements;i++)
+        {
+            pointToHead->head = union_find->elements[toHost].head; 
+            if (pointToHead->next != null)
+                pointToHead = pointToHead->next;
+        }
+         union_find->elements[toHost].head.numberElements += pointToSentinel->numberElements;
+         pointToSentinel->first = NULL;
+         pointToSentinel->last = NULL; 
+
+         return UF_MERGED;
+    }
+}
